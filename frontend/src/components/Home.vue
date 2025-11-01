8474b62b956fce0d9153d8be2971e0aa2990b5d0
Diana <doganesyan12@gmail.com>
2025-11-01 14:10:12 +0300

    second commit

--- frontend/src/components/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="container">
-        <h1>Welcome</h1>
-        <button class="google-btn" @click="LoginWithGoogle">
-            <img src="../assets/google.webp" width="32"/>
-            Login with google
-        </button>
-    </div>
-</template>
