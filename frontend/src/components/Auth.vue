519365bf31c1d5c3f7f2c7b3243078644393aaf8
Diana <doganesyan12@gmail.com>
2025-11-01 18:44:53 +0300

    third commit

---
+++ frontend/src/components/Auth.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h1>Welcome</h1>
+        <button class="google-btn" @click="LoginWithGoogle">
+            Login with google
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  methods: {
+    LoginWithGoogle() {
+      console.log('Google login clicked')
+    }
+  }
+}</script>
